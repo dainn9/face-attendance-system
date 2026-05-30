6d8bd1436f936e8600b1081cc110a5dec77dd66c
Thanh Danh <thanhdanh431@gmail.com>
2026-05-30 14:06:00 +0700

    refactor(repository): remove unused faculty error codes and update user code checks

--- apps/server/exam-proctoring/services/user-service/Infrastructure/Persistence/Repositories/UserRepository.cs
+++ apps/server/exam-proctoring/services/user-service/Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using user_service.Application.Abstractions.Persistence;
-using user_service.Application.Contracts;
 using user_service.Domain.Aggregates.User;
 
 namespace user_service.Infrastructure.Persistence.Repositories
@@ -14,34 +13,14 @@ namespace user_service.Infrastructure.Persistence.Repositories
         public void Add(User user)
         => _context.Users.Add(user);
 
-        public Task<UserDto?> GetProfileByIdAsync(Guid userId, CancellationToken cancellationToken)
-        => _context.Users
-            .AsNoTracking()
-            .Where(u => u.Id == userId)
-            .Select(u => new UserDto(
-                u.Id,
-                u.FullName,
-                u.Gender,
-                u.DateOfBirth,
-                u.Email,
-                u.Role,
-
-                u.StudentProfile != null ? u.StudentProfile.StudentCode : null,
-
-                u.StudentProfile != null ? u.StudentProfile.ClassCode : null,
-
-                u.LecturerProfile != null ? u.LecturerProfile.FacultyCode : null
-            ))
-            .FirstOrDefaultAsync(cancellationToken);
-
         public Task<bool> ExistsByIdAsync(Guid userId, CancellationToken cancellationToken)
         => _context.Users
             .AsNoTracking()
             .AnyAsync(u => u.Id == userId, cancellationToken);
 
-        public Task<bool> ExistsByStudentCodeAsync(string studentCode, CancellationToken cancellationToken)
+        public Task<bool> ExistsByUserCodeAsync(string userCode, CancellationToken cancellationToken)
         => _context.Users
             .AsNoTracking()
-            .AnyAsync(u => u.StudentProfile != null && u.StudentProfile.StudentCode == studentCode, cancellationToken);
+            .AnyAsync(u => u.UserCode == userCode, cancellationToken);
     }
 }
