f8086dc23d501bd76fcaf6403b1e477494e80a2a
Thanh Danh <thanhdanh431@gmail.com>
2026-06-06 21:40:43 +0700

    feat(enrollment): add endpoint to add enrollments to a course section

--- apps/server/exam-proctoring/services/attendance-service/Infrastructure/Persistence/Repositories/CourseSectionRepository.cs
+++ apps/server/exam-proctoring/services/attendance-service/Infrastructure/Persistence/Repositories/CourseSectionRepository.cs
@@ -1,5 +1,6 @@
 using attendance_service.Application.Abstractions.Persistence;
 using attendance_service.Domain.Aggregates.CourseSection;
+using Microsoft.EntityFrameworkCore;
 
 namespace attendance_service.Infrastructure.Persistence.Repositories
 {
@@ -11,5 +12,10 @@ namespace attendance_service.Infrastructure.Persistence.Repositories
             _context = Context;
         public void Add(CourseSection courseSection)
         => _context.CourseSections.Add(courseSection);
+
+        public Task<CourseSection?> GetWithEnrollmentsByIdAsync(Guid courseSectionId, CancellationToken cancellationToken)
+        => _context.CourseSections
+            .Include(cs => cs.Enrollments)
+            .FirstOrDefaultAsync(cs => cs.Id == courseSectionId, cancellationToken);
     }
 }
